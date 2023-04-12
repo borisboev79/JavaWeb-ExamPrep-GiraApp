8944f280d1413c57e67398e09d10d75aa42893c0
Boris Boev <boris.boev@gmail.com>
2023-04-12 11:51:16 +0300

    First Commit

---
+++ src/main/java/com/example/giraapp/GiraAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.giraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiraAppApplication.class, args);
+    }
+
+}
